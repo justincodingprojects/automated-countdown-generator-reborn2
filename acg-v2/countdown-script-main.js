3a537f477f957aa8efffda63d549c158fb3be94a
Justin Programming Skills <90016710+justincodingprojects@users.noreply.github.com>
2023-09-01 12:35:33 -0400

    Update countdown-script-main.js

--- acg-v2/countdown-script-main.js
+++ acg-v2/countdown-script-main.js
@@ -41,6 +41,7 @@ function updateCountdown() {
         }
 
         // Update countdown display
+        document.getElementById("title").innerText = blocks[currentBlockIndex].block;
         document.getElementById("demo").innerText = formatDuration(timeRemaining);
     }
 }
